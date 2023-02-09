02338c841e7fee624e7c33a9d15608089db4d83f
Jun99uu <igun0423@naver.com>
2023-02-09 19:05:19 +0900

    add : 버튼 추가

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color(0xFF181818),
           body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(children: [
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               SizedBox(
-                height: 80,
+                height: 50,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
@@ -22,21 +23,69 @@ class MyApp extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Text("Hey JunkyuLee",
-                          style: TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.w800,
-                            color: Colors.white,
-                          )),
-                      Text("Welcome back",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white.withOpacity(0.8),
-                          ))
+                      Text(
+                        "Hey JunkyuLee",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ),
                     ],
                   )
                 ],
-              )
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Text(
+                "Total Balance",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "\$5 194 482",
+                style: TextStyle(
+                    fontSize: 42,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(45),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(
+                          color: Color(0xFF181818),
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              ])
             ]),
           )),
     );
